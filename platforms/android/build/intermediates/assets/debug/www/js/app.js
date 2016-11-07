8e69760102dd457a0fb027f1e5e682df6ed868c8
michaeljymsgutierrez <michael@vielsoft.com>
2016-11-07 17:10:04 +0800

    update

--- platforms/android/build/intermediates/assets/debug/www/js/app.js
+++ platforms/android/build/intermediates/assets/debug/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
- var app = angular.module('konnekt', ['ionic','ngCordova']); //,'ngCordovaBeacon'
+ var app = angular.module('konnekt', ['ionic','ngCordova','ion-floating-menu']); //,'ngCordovaBeacon'
 
 
 app.run(function($ionicPlatform,$cordovaBeacon) {
