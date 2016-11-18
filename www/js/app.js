b57c68a7f75a390dc7b684d5098f1b97f6981f07
michaeljymsgutierrez <michael@vielsoft.com>
2016-11-18 16:08:05 +0800

    add service for removing views cache and history

--- www/js/app.js
+++ www/js/app.js
@@ -1,39 +1,36 @@
  var app = angular.module('konnekt', ['ionic','ngCordova','ion-floating-menu']);
 
 
-app.run(function($ionicPlatform,$cordovaBeacon,$window) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+app.run(function($ionicPlatform,$cordovaBeacon,$window,clearAppDataCache){
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-       StatusBar.show();
-    }
+  $ionicPlatform.ready(function(){
 
-    $ionicPlatform.registerBackButtonAction(function(e){
-      backAsHome.trigger(function(){
-          console.log("Success Over riding Back as Home . . .");
-      }, function(){
-          console.log("Error Over riding Back as Home . . .");
-      });
-      e.preventDefault();
-    },101);
+        //Clear AppDataCache service
+        clearAppDataCache.clearApp();
 
-    //Enable the app to autostart
-    cordova.plugins.autoStart.enable();
+        if(window.cordova && window.cordova.plugins.Keyboard){
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
 
-    //Enable autostart Bluetooth
-    $cordovaBeacon.enableBluetooth();
+        if(window.StatusBar){
+             StatusBar.show();
+        }
 
-    //Disable autostart Bluetooth
-    //$cordovaBeacon.disableBluetooth();
+        $ionicPlatform.registerBackButtonAction(function(e){
+          backAsHome.trigger(function(){
+              console.log("Success Over riding Back as Home . . .");
+          }, function(){
+              console.log("Error Over riding Back as Home . . .");
+          });
+          e.preventDefault();
+        },101);
+
+        //Enable the app to autostart
+        cordova.plugins.autoStart.enable();
+
+        //Enable autostart Bluetooth
+        $cordovaBeacon.enableBluetooth();
 
   });
 
