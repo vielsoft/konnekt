1846d7462d9ef6270498d2d81b8d2871d57520fc
Michael Jyms Gutierrez <michael@vielsoft.com>
2016-12-19 12:06:31 +0800

    change fulldate_detected from text to date in table creation

--- www/js/app.js
+++ www/js/app.js
@@ -38,7 +38,7 @@ app.run(function($ionicPlatform,$cordovaBeacon,$window,clearAppDataCache,$cordov
         db = $cordovaSQLite.openDB({name:'konnekt.db',location:'default'});
         //db = window.openDatabase("konnekt.db", "1.0", "Cordova Demo", 200000);
         //var db = $cordovaSQLite.openDB({name:'konnekt.db',bgType:1,location:'default'});
-        $cordovaSQLite.execute(db,"CREATE TABLE IF NOT EXISTS konnekt_table (id text primary key , title  text , text text , url text , icons text , content text , fulldate_detected text , date_detected text)")
+        $cordovaSQLite.execute(db,"CREATE TABLE IF NOT EXISTS konnekt_table (id text primary key , title  text , text text , url text , icons text , content text , fulldate_detected date , date_detected text)")
         .then(function(res){
             console.log("konnekt_table has been created . . . ");
         },function(err){
